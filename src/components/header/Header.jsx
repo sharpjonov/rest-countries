901945cf9f5fe78f06ab5cc575244111a5ec05eb
sharpjonov <muhammaduka@gmail.com>
2025-08-04 19:33:25 +0500

    dark mode added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,29 @@
 import "./header.css";
 import moon from "../../assets/images/half-moon.svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [theme, setTheme] = useState(false);
+  const handleBtn = () => {
+    setTheme(!theme);
+  };
   return (
     <>
-      <header>
+      <header className={theme ? "bg-black text-white" : "bg-white text-black"}>
         <div className="container">
           <div className="header__inner">
             <div className="header__left">
               <h1 className="header__left__title">Where in the world?</h1>
             </div>
             <div className="header__right">
-              <img src={moon} width={16} height={16} />
-              <p className="header__right__link">Dark Mode</p>
+              <button
+                onClick={handleBtn}
+                className="header__right__link flex items-center gap-[8px]"
+              >
+                <img src={moon} width={16} height={16} />
+                Dark Mode
+              </button>
             </div>
           </div>
         </div>
